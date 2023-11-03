3767596f6a4a3080388e0e1c2acf8048054c6630
Dmytro Mayboroda <mayboroda.de@gmail.com>
2023-11-03 11:58:20 +0100

    Renaming lib to http

---
+++ http/src/lib.rs
@@ -0,0 +1,55 @@
+use std::io::stdin;
+
+#[derive(Debug, PartialEq)]
+pub struct HttpHeader {
+    name: String,
+    value: String
+}
+
+impl HttpHeader {
+    fn new(n: &str, v:&str) -> Self {
+        HttpHeader {
+            name: String::from(n),
+            value: String::from(v)
+        }
+    }
+
+    fn parse(line: &String) -> Option<Self> {
+        line.trim().split_once(":")
+            .map(|p| Self::new(p.0.trim(), p.1.trim()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::HttpHeader;
+
+    #[test]
+    fn parse_headers_successfully() {
+        let headers = [
+            ("Content-Type: application/json", Some(HttpHeader{name: "Content-Type".to_string(), value: "application/json".to_string()})),
+            ("content-type : application/json", Some(HttpHeader{name: "content-type".to_string(), value: "application/json".to_string()})),
+            (" content-Type : application/json ", Some(HttpHeader{name: "content-Type".to_string(), value: "application/json".to_string()})),
+        ];
+
+        let mods = headers.map(|h| (HttpHeader::parse(&String::from(h.0)), h.1));
+
+        for case in mods {
+            assert_eq!(case.0, case.1)
+        }
+    }
+}
+fn main() {
+    loop {
+        let mut input = String::new();
+
+        match stdin().read_line(&mut input) {
+            Ok(_) => {
+                if let Some(header) = HttpHeader::parse(&input) {
+                    println!("header is {:?}", header)
+                }
+            }
+            Err(_) => break
+        }
+    }
+}
